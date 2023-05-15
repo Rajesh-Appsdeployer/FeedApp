c913435c8e2d8a6e1ce858cf481f2a8b68904bdb
Rajesh <rajesh11022002@gmail.com>
2023-05-15 15:54:56 +0530

    some changes in post

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,30 +3,20 @@ import profile_image from '../images/images.jpeg'
 import '../Css/createpost.css'
 import image2 from '../images/image2.webp'
 import '../Css/post.css'
+// import BlogData from "../components/data";
  
-// import { FaHireAHelper, FaPoll } from 'react-icons/fa'
-// import { CgFileDocument } from 'react-icons/cg'
-// import { MdOutlineAddAPhoto, MdCelebration } from 'react-icons/md'
-// import { FiVideo } from 'react-icons/fi'
-// import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-// import Tooltip from 'react-bootstrap/Tooltip';
-// import Overlay from 'react-bootstrap/Overlay';
-// import Popover from 'react-bootstrap/Popover';
+ 
 const FileUploader = () => {
-    // const hiddenFileInput = React.useRef(null);
-    // const [target, setTarget] = useState(null);
-    // const ref = useRef(null);
-    // const handleClickSelect = event => {
-    //     console.log("click")
-    //     hiddenFileInput.current.click();
-    // };
-    // const [input, setInput] = useState("");
-    // const [showEmojis, setShowEmojis] = useState(false);
+     
+    const [input, setInput] = useState("");
+    const [des,setDes]= useState('')
+    //  const handlechange=()=>{
+    //   BlogData.push({
 
-    // const handleClick = (event) => {
-    //     setShowEmojis(!showEmojis);
-    //     setTarget(event.target);
-    // };
+    //   })
+    //  }
+
+     
     return (
         <>
             <div className='form'>
